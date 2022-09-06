cea0d9ae2001eb2363a1a1288cdfb6a7f7db015e
CrystallineButterfly <95975209+CrystallineButterfly@users.noreply.github.com>
2022-09-06 10:19:36 +0100

    Add static home page

---
+++ my-app/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+//import Footer from './Footer';
+import Intro from "./components/Intro";
+import MidSection from "./components/MidSection";
+
+const index = () => {
+  return (
+    <div>
+      <Intro />
+      <MidSection />
+    </div>
+  );
+};
+
+export default index;
