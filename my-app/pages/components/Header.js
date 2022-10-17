70e2e717bf0c16e342f5c4f710b9c76373da8247
Caleb Moses HYMELE TCHELIBOU <moses@Claudes-MacBook-Pro.local>
2022-10-17 14:29:58 +0100

    styling

--- my-app/pages/components/Header.js
+++ my-app/pages/components/Header.js
@@ -7,16 +7,16 @@ function Header() {
 
   return (
     // <nav className="flex mt-3 justify-between">
-    <nav className="flex justify-between items-center px-4">
+    <nav className="flex justify-evenly text-white items-center px-4">
       <div className="flex m-3 gap-5 items-center">
         <ThemeToggler />
-        <button className="border rounded-full p-3">
+        <button className="border text-white border-gray-200 rounded-full p-3">
           Treasury <i className="ri-bar-chart-horizontal-line bg-inherit"></i>{" "}
           42,000
         </button>
       </div>
       <script defer src="app.js"></script>
-      <div className="flex gap-4">
+      <div className="flex gap-16">
         <button onClick={() => router.push("/components/Dao", "/Dao")}>
           DAO
         </button>
@@ -29,8 +29,10 @@ function Header() {
         </button>
         <button onClick={() => router.push("/")}>Community</button>
         <button onClick={() => router.push("/")}>Capsule</button>
-        <ConnectButton />
       </div>
+      <div>
+        <ConnectButton />
+      </div>    
     </nav>
   );
 }
