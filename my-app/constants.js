e4acfbb57e098fb51dc855dbc3f51b1a5ff21462
CrystallineButterfly <95975209+CrystallineButterfly@users.noreply.github.com>
2022-10-19 14:45:03 +0100

    Add constants.js file for contract addresses and abis

---
+++ my-app/constants.js
@@ -0,0 +1,10 @@
+
+export const DAO_Executor_CONTRACT_ADDRESS =
+  "";
+export const Founderz_DAO_Logic_CONTRACT_ADDRESS =
+  "";
+export const Founderz_NFT_CONTRACT_ADDRESS = "";
+export const NFT_Auction_House_CONTRACT_ADDRESS = "";
+
+// ABIS BELOW
+
