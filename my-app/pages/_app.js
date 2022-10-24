31db0f365ccbacf2b42b2ba41113c8051acc92e2
CrystallineButterfly <95975209+CrystallineButterfly@users.noreply.github.com>
2022-10-24 15:42:22 +0100

    Add fix for deploy

--- my-app/pages/_app.js
+++ my-app/pages/_app.js
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import type, { AppProps } from "next/app";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import { useRouter } from "next/router";
 import "@rainbow-me/rainbowkit/styles.css";
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
