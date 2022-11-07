6b4c0e1fb4f5818a942f39fce8cedd2167a3c701
CrystallineButterfly <95975209+CrystallineButterfly@users.noreply.github.com>
2022-11-07 13:33:16 +0000

    Add fixes and changes

--- my-app/pages/components/Proposal.js
+++ my-app/pages/components/Proposal.js
@@ -1,3 +1,6 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable react/jsx-key */
 import Header from './Header'
 
