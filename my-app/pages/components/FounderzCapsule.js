240ef95a2a409f46bd5c5c6942e6f663e6a5c728
CrystallineButterfly <95975209+CrystallineButterfly@users.noreply.github.com>
2022-11-07 13:58:14 +0000

    Add fixes for deployment-tests v4

--- my-app/pages/components/FounderzCapsule.js
+++
@@ -1,9 +0,0 @@
-// import * as THREE from "three";
-// import { Contract, providers } from "ethers";
-// import { formatEther } from "ethers/lib/utils";
-// // import {
-// //   // (abis also)
-// //   Founderz_NFT_CONTRACT_ADDRESS
-// // } from "../constants";
-
-// const FounderzCapsule = () => {};
