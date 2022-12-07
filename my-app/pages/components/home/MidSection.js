27bb695cef188f3cb64c54808df4d64b70f23323
Caleb Moses HYMELE TCHELIBOU <moses@Claudes-MacBook-Pro.local>
2022-12-07 23:03:13 +0100

    working on faq section

--- my-app/pages/components/home/MidSection.js
+++ my-app/pages/components/home/MidSection.js
@@ -77,10 +77,10 @@ const MidSection = () => {
 
         <div className="mb-28">
           <h2 className="text-center my-5 text-3xl font-bold text-[#160744] dark:text-white font-[all-round-gothic]">FAQ</h2>
-          <p className="text-[#4965D8]">SUMMARY</p>
+          <p className="text-[#4965D8] font-[all-round-gothic]">SUMMARY</p>
           <ul className="grid lg:grid-cols-2 grid-cols-1 gap-2 ">
             {summary.map((i) => (
-              <li className="flex  text-[#8094B7] font-[200]">
+              <li className="flex  text-[#8094B7] dark:text-[#F7F9FC] font-[200]">
                 <div className="w-4 pt-[10px]"><div className="h-[5px] w-[5px]  rounded-full mr-2 bg-[#4965D8]" /></div> {i}
               </li>
             ))}
