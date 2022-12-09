cab4c79e80d0eb3b1201326bc1f92f9e5b79e85f
Kell McWatt <fromtc2k42@gmail.com>
2022-12-09 13:59:50 +0000

    Add fixes and first consts for functionality v1

--- my-app/pages/components/Intro.js
+++ my-app/pages/components/Intro.js
@@ -11,7 +11,7 @@ import {
   Founderz_NFT_ABI,
   Auction_House_ABI,
   Auction_House_CONTRACT_ADDRESS,
-} from "../constants";
+} from "/my-app/constants.js";
 import Carousel from 'react-material-ui-carousel';
 import founderzpass from '../assets/founderzpass.png';
 import founderzstand from '../assets/founderzstand.png';
