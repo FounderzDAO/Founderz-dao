71d942cfe513c86f20ecb1e3014c874e66754d7c
K42 Ψ <fromtc2k42@gmail.com>
2023-04-25 16:04:27 +0100

    Add updates

--- my-app/pages/components/home/IntroPage.js
+++ my-app/pages/components/home/IntroPage.js
@@ -178,7 +178,7 @@ const IntroPage = () => {
               }}
             />
             {/* Map this arrow to display past ids and other data (auction call) */}
-            <div className=" w-7/12 sm:w-full flex flex-col items-center animate-pulse">
+            <div className=" w-7/12 sm:w-full flex flex-col items-center">
               <img src="img/fdpass-1.png" />
               <img src="img/founderzstand.png" />
             </div>
