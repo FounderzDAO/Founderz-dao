10f3b2d7a3b1a04661d25d05cc11939721a6d094
K42 Ψ <fromtc2k42@gmail.com>
2023-05-05 19:58:12 +0100

    Update hardhat vars for mana(gas) testing

--- hardhat-environment/hardhat.config.js
+++ hardhat-environment/hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-verify");
+require("hardhat-gas-reporter");
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
