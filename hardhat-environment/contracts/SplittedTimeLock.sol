8a8fa1dbade54b24e4464911d16b74b6709290e3
K42 Ψ <fromtc2k42@gmail.com>
2023-05-20 02:38:30 +0100

    Add update contract-depz for founderzNft

--- hardhat-environment/contracts/SplittedTimeLock.sol
+++ hardhat-environment/contracts/SplittedTimeLock.sol
@@ -2,7 +2,7 @@
 
 pragma solidity 0.8.19;
 
-import "./TimelockController.sol";
+import "./dependencies/TimelockController.sol";
 
 contract MyTimelock is TimelockController {
     constructor(uint256 delay, address[] memory proposers, address[] memory executors, address admin) TimelockController(delay, proposers, executors, admin) {}
